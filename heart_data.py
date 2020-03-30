0a0ccbd6ec0f2035ee4c9e19f74538411b19993d
Tyler Brennan <58308659+tbrennan1271@users.noreply.github.com>
2020-03-30 19:53:43 -0400

    Cleaned code and added names

--- heart_data.py
+++ heart_data.py
@@ -1,3 +1,8 @@
+'''
+Aidan Lee and Tyler Brennan
+ENGR 306
+Professor Ning
+'''
 
 import csv
 import copy
@@ -14,49 +19,6 @@ TEST_PERCENT = 0.20
 LABEL = 'num'
 GROUP_CAP = 5           # Cap for the number of groups used to determine information gain
 MAX_LABEL = 0             # Determined from code
-'''
-class value:
-    def __init__(self, data_arr, attributes):
-        self.data = {}
-        for i in range(len(attributes)):
-            self.data[attributes[i]] = data_arr[i]
-
-# In: name: file name  &  label: True if the data contains labels for data, and False if regular data
-# Out: data: array that contains all the data
-# Out: data_label: array of lables for data
-def get_data(name):
-    global DIM
-    global K
-    data = []
-    attributes = []
-    with open(name, newline='', encoding='utf-8-sig') as csvfile:
-        reader = csv.reader(csvfile, delimiter=',')
-        for entry in reader:
-            DIM = len(entry)
-            temp = []
-            for value in entry:
-                if(value == '?'):
-                    temp.append(value)
-                else:
-                    try:
-                        temp.append(float(value))
-                    except:
-                        attributes.append(value)
-            if len(temp) != 0:
-                data.append(temp)
-    data_array(data, attributes)
-    return data
-
-
-def data_array(data, attributes):
-    data_array = []
-    for entry in data:
-        p1 = value(entry, attributes)
-        data_array.append(p1)
-    print(data_array[0].data)
-    return data_array
-        
-'''
 
 def data_split(input_data, test_percent):
     training_data = copy.copy(input_data)
